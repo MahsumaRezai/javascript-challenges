ad8a1cb50d81d08f7c4dde8b2b121645c7ddb2bb
Masoumeh Rezai <you@example.com>
2023-05-02 01:42:52 +0430

    string slouation

--- app/app.js
+++
@@ -1,6 +0,0 @@
-function str(s) {
-    return s.split('').reduce((a, b) => {
-        return b + a;
-    })
-}
-str();
--- app/string_reverse/string_reverse-slouction#3.js
+++ app/string_reverse/string_reverse-slouction#3.js
@@ -1,9 +1,7 @@
-function str(s) {
-    let revers = '';
-    for (let char of s) {
-        revers = char + revers;
-    }
-    return revers;
 
+function str(s) {
+    return s.split('').reduce((a, b) => {
+        return b + a;
+    })
 }
 str();
