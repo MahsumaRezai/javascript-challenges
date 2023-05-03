68461f810502c5bada07d318cfb948eef0b82f13
Masoumeh Rezai <you@example.com>
2023-05-03 07:06:55 +0430

    rest and spered

--- app/The Classic FizzBuzz!/The Classic FizzBuzz!.js
+++ app/The Classic FizzBuzz!/The Classic FizzBuzz!.js
@@ -1,3 +1,4 @@
+
 function fizzbuzz(n) {
     for (let i = 1; i <= n; i++) {
         if (i % 3 == 0 && i % 5 == 0) {
@@ -14,4 +15,4 @@ function fizzbuzz(n) {
     }
 
 }
-fizzbuzz()
+fizzbuzz();
---
+++ app/app.js
@@ -0,0 +1,6 @@
+let arr = [1, 2, 3, 67];
+[a, b, a, b] = arr;
+console.log(a);
+console.log(b)
+
+
